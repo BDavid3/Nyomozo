d48b527f350ab4c1195c50fdf5f8a91e26fe6fe8
user <user@gmail.com>
2026-02-13 09:58:08 +0100

    UI megcsinálása

--- Nyomozo/FőProgram.cs
+++ Nyomozo/FőProgram.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UI();
+			int intSzamInput;
+
+			do
+			{
+				Console.Write("Szám: ");
+				intSzamInput = Convert.ToInt32(Console.ReadLine());
+
+				switch (intSzamInput)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+					case 6:
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Rossz értéket adtál meg!");
+						break;
+				}
+			}
+			while (intSzamInput < 1 || intSzamInput > 6);
+			
+            
+		}
+
+        static void UI()
+        {
+			Console.WriteLine("1. Ügyek kezelése");
+			Console.WriteLine("2. Személyek kezelése");
+			Console.WriteLine("3. Bizonyítékok kezelése");
+			Console.WriteLine("4. Idővonal megtekintése");
+			Console.WriteLine("5. Elemzés / Döntések");
+			Console.WriteLine("6. Kilépés\n");
         }
     }
 }
